9b49640cc8c77fedf7d953339c8b5c67103057d4
sivaldo vieira <sivaldovieiradealmeida@gmail.com>
2024-12-31 20:28:33 -0300

    Criando a primeira página do sistema

--- magazineHashtagR/src/App.jsx
+++ magazineHashtagR/src/App.jsx
@@ -1,11 +1,14 @@
 import Header from "./components/Header";
+import Home from "./pages/HomePage/Home";
 
 function App() {
- 
   return (
-   <Header />
+   <>
+     <Header />
+     <Home />
+   </>
   ) 
     
 }
 
-export default App
+export default App;
