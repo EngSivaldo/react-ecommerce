4caca6eb73da3ac6b9856b82e25ea45b59ec5b16
sivaldo vieira <sivaldovieiradealmeida@gmail.com>
2025-01-01 13:13:20 -0300

    A caixa de filtros de produtos

--- magazineHashtagR/src/components/UserButtons.jsx
+++ magazineHashtagR/src/components/UserButtons.jsx
@@ -4,9 +4,12 @@ import { faBagShopping, faUser, faCartShopping } from '@fortawesome/free-solid-s
 const UserButtons = () => {
   return (
     <div>  
-      <button className='px-2'><FontAwesomeIcon icon={faCartShopping} /></button>
+      <button className='px-2 relative'>
+        <FontAwesomeIcon icon={faCartShopping} className='fa-2x'/>
+        <div id='cart-amount ' className='absolute inline-flex items-center justify-center w-5 h-5 font-semibold text-white-500 bg-red-500 border-2 border-white rounded-full -top-2 -right-2'>3</div>
+      </button>
       <a className='px-2'>
-        <FontAwesomeIcon icon={faUser} />
+        <FontAwesomeIcon icon={faUser} className='fa-2x'/>
       </a>
     </div>
   );
