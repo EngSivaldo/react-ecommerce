e56a0295c9e3350b179dac7399a1668abbd5ea23
sivaldo vieira <sivaldovieiradealmeida@gmail.com>
2025-01-01 18:50:00 -0300

    componente Link

--- magazineHashtagR/src/components/Header.jsx
+++ magazineHashtagR/src/components/Header.jsx
@@ -1,24 +1,24 @@
 import { faBusSimple } from '@fortawesome/free-solid-svg-icons';
 import logo from '../assets/logo/hashtag.svg';
 import UserButtons from './UserButtons';
-import { useLocation } from 'react-router-dom';
+import { useLocation, Link } from 'react-router-dom';
 
 const Header = () => {
   const { pathname } = useLocation();
   const completeHeader = (
     <header className='flex sticky top-0 shadow-xl shadow-slate-400 bg-slate-950 text-slate-200 px-8 py-4 items-end justify-between text-4-lg z-10'>
-      <a href="/">
+      <Link to="/">
         <img className='h-16 px-2' src={logo} alt="Logo da Hashtag." />
-      </a>
+      </Link>
       <UserButtons/>
     </header>
     );
 
   const SimpleHeader = (
     <header className='flex sticky top-0 shadow-xl shadow-slate-400 bg-slate-950 text-slate-200 px-8 py-4 items-end justify-between text-4-lg z-10'>
-      <a href="/">
+      <Link to="/">
         <img className='h-16 px-2' src={logo} alt="Logo da Hashtag." />
-      </a>
+      </Link>
     </header>
     );
   return <>{pathname === '/' ? completeHeader : SimpleHeader} </>; 
--- magazineHashtagR/src/components/UserButtons.jsx
+++ magazineHashtagR/src/components/UserButtons.jsx
@@ -1,5 +1,6 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBagShopping, faUser, faCartShopping } from '@fortawesome/free-solid-svg-icons';
+import { Link } from 'react-router-dom';
 
 const UserButtons = () => {
   return (
@@ -8,9 +9,9 @@ const UserButtons = () => {
         <FontAwesomeIcon icon={faCartShopping} className='fa-2x'/>
         <div id='cart-amount ' className='absolute inline-flex items-center justify-center w-5 h-5 font-semibold text-white-500 bg-red-500 border-2 border-white rounded-full -top-2 -right-2'>3</div>
       </button>
-      <a className='px-2'>
+      <Link to='/history' className='px-2'>
         <FontAwesomeIcon icon={faUser} className='fa-2x'/>
-      </a>
+      </Link>
     </div>
   );
 };
