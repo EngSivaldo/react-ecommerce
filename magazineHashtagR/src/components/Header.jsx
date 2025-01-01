db63d1761368563059731db52d4034f2da7a9872
sivaldo vieira <sivaldovieiradealmeida@gmail.com>
2025-01-01 18:42:45 -0300

    O Roteamento da aplicação,header Dinamico

--- magazineHashtagR/src/components/Header.jsx
+++ magazineHashtagR/src/components/Header.jsx
@@ -1,15 +1,28 @@
+import { faBusSimple } from '@fortawesome/free-solid-svg-icons';
 import logo from '../assets/logo/hashtag.svg';
 import UserButtons from './UserButtons';
+import { useLocation } from 'react-router-dom';
 
 const Header = () => {
-  return (
-  <header className='flex sticky top-0 shadow-xl shadow-slate-400 bg-slate-950 text-slate-200 px-8 py-4 items-end justify-between text-4-lg z-10'>
-    <a href="/">
-      <img className='h-16 px-2' src={logo} alt="Logo da Hashtag." />
-    </a>
-    <UserButtons/>
-  </header>
-  )
+  const { pathname } = useLocation();
+  const completeHeader = (
+    <header className='flex sticky top-0 shadow-xl shadow-slate-400 bg-slate-950 text-slate-200 px-8 py-4 items-end justify-between text-4-lg z-10'>
+      <a href="/">
+        <img className='h-16 px-2' src={logo} alt="Logo da Hashtag." />
+      </a>
+      <UserButtons/>
+    </header>
+    );
+
+  const SimpleHeader = (
+    <header className='flex sticky top-0 shadow-xl shadow-slate-400 bg-slate-950 text-slate-200 px-8 py-4 items-end justify-between text-4-lg z-10'>
+      <a href="/">
+        <img className='h-16 px-2' src={logo} alt="Logo da Hashtag." />
+      </a>
+    </header>
+    );
+  return <>{pathname === '/' ? completeHeader : SimpleHeader} </>; 
+  
 };
 
 
---
+++ magazineHashtagR/src/pages/CheckoutPage/Checkout.jsx
@@ -0,0 +1,6 @@
+const Checkout = () => {
+   return <h1>Bem vindo à página de checkout!</h1>
+};
+
+
+export default Checkout;
