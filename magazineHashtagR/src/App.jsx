8209d23326cfafe9bf68a9a3ec2e31f0ddf5092d
sivaldo vieira <sivaldovieiradealmeida@gmail.com>
2025-01-03 18:52:13 -0300

    Página principal - refatorando o Contexto do Carrinho

--- magazineHashtagR/src/App.jsx
+++ magazineHashtagR/src/App.jsx
@@ -1,63 +1,22 @@
-import { useState } from "react";
 import Header from "./components/Header";
 import Home from "./pages/HomePage/Home";
 import Checkout from "./pages/CheckoutPage/Checkout";
 import PurchaseHistory from "./pages/PurchaseHistoryPage/PurchaseHistory";
 import { Routes, Route } from "react-router-dom";
-import { CartContext } from "./context/CartContext";
+import CartContextProvider from "./context/CartContext"; // Importando CartContextProvider
 
-// Componente principal do aplicativo
 function App() {
-  // Estado para controlar se o carrinho está aberto
-  const [isCartOpen, setIsCartOpen] = useState(false);
-  // Estado para armazenar os itens do carrinho
-  const [cartItems, setCartItems] = useState({});
-
-  // Função para alternar o estado de abertura do carrinho
-  const toggleIsCartOpen = () => {
-    setIsCartOpen(!isCartOpen);
-  };
-
-  // Função para remover um item do carrinho
-  const removeFromCart = (productId) => {
-    const cartItemsCopy = { ...cartItems };
-    delete cartItemsCopy[productId];
-    setCartItems(cartItemsCopy);
-  };
-
-  // Função para adicionar um item ao carrinho
-  const addToCart = (productId) => {
-    const updatedCart = { 
-      ...cartItems, 
-      [productId]: (cartItems[productId] ?? 0) + 1,
-    };
-    setCartItems(updatedCart);
-  };
-
-  // Função para diminuir a quantidade de um item no carrinho
-  const decreaseUnit = (productId) => {
-    if (cartItems[productId] > 1) {
-      const updatedCart = { 
-        ...cartItems, 
-        [productId]: cartItems[productId] - 1,
-      };
-      setCartItems(updatedCart);
-    } else {
-      removeFromCart(productId);
-    }
-  };
+ 
 
   return (
-    <CartContext.Provider value={{ isCartOpen, toggleIsCartOpen, cartItems, setCartItems, addToCart, decreaseUnit, removeFromCart }}>
-      {/* Componente do cabeçalho */}
+    <CartContextProvider>
       <Header />
-      {/* Definição das rotas do aplicativo */}
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/checkout' element={<Checkout />} />
         <Route path='/history' element={<PurchaseHistory />} />
       </Routes>
-    </CartContext.Provider>
+    </CartContextProvider>
   );
 }
 
---
+++ magazineHashtagR/src/context/CartContext.jsx
@@ -0,0 +1,64 @@
+import { useState, createContext } from "react";
+
+// Contexto do carrinho
+export const CartContext = createContext(null);
+
+// Função para obter a quantidade de itens no carrinho
+export const getAmountOfItemsInCart = (cartItemsObj) => {
+  let amount = 0;
+  for (const productId in cartItemsObj) {
+    amount += cartItemsObj[productId];
+  }
+  return amount;
+};
+
+// Usar essa função no UserButtons
+
+const CartContextProvider = ({ children }) => {
+  // Estado para controlar se o carrinho está aberto
+  const [isCartOpen, setIsCartOpen] = useState(false);
+  // Estado para armazenar os itens do carrinho
+  const [cartItems, setCartItems] = useState({});
+
+  // Função para alternar o estado de abertura do carrinho
+  const toggleIsCartOpen = () => {
+    setIsCartOpen(!isCartOpen);
+  };
+
+  // Função para remover um item do carrinho
+  const removeFromCart = (productId) => {
+    const cartItemsCopy = { ...cartItems };
+    delete cartItemsCopy[productId];
+    setCartItems(cartItemsCopy);
+  };
+
+  // Função para adicionar um item ao carrinho
+  const addToCart = (productId) => {
+    const updatedCart = {
+      ...cartItems,
+      [productId]: (cartItems[productId] ?? 0) + 1,
+    };
+    setCartItems(updatedCart);
+  };
+
+  // Função para diminuir a quantidade de um item no carrinho
+  const decreaseUnit = (productId) => {
+    if (cartItems[productId] > 1) {
+      const updatedCart = {
+        ...cartItems,
+        [productId]: cartItems[productId] - 1,
+      };
+      setCartItems(updatedCart);
+    } else {
+      removeFromCart(productId);
+    }
+  };
+
+  return (
+    <CartContext.Provider value={{ isCartOpen, cartItems, toggleIsCartOpen, removeFromCart, addToCart, decreaseUnit }}>
+      {children}
+    </CartContext.Provider>
+  );
+};
+
+export default CartContextProvider;
