6585c244fa655c71452d190705371ba9b4cd3c56
sivaldo vieira <sivaldovieiradealmeida@gmail.com>
2025-01-04 15:48:03 -0300

    Adaptando o componente de item do carrinho

--- magazineHashtagR/src/pages/CheckoutPage/Checkout.jsx
+++ magazineHashtagR/src/pages/CheckoutPage/Checkout.jsx
@@ -1,16 +1,21 @@
 const Checkout = () => {
    return (
-
-      <main className="bg-stone-200 h-[calc(1000dvh-5rem)]">
+      <main className="bg-stone-200 h-[calc(100vh-5rem)]">
          <p className="text-center text-2xl font-bold text-slate-950 pt-8">
-         Finalizar Compra!  
-         </p> 
+            Finalizar Compra!
+         </p>
+         <form className="grid grid-rows-5 grid-cols-3 grid-flow-col gap-4 h-3/4 mt-5 px-8">
+            <p className="bg-red-700">elementos 1</p>
+            <p className="bg-red-700">elementos 1</p>
+            <p className="bg-red-700">elementos 1</p>
+            <p className="bg-red-700">elementos 1</p>
+            <p className="bg-red-700">elementos 1</p>
+            <p className="bg-red-700">elementos 1</p>
+            <p className="bg-red-700">elementos 1</p>
 
-     </main>
-
-   )
-   
+         </form>
+      </main>
+   );
 };
-
 
 export default Checkout;
